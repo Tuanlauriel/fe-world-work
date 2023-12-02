f0bd9334c62c2353efedc77354bb7ca69e575d7f
Nguyen Minh Hieu <ngmhieu1709@gmail.com>
2023-12-02 14:11:38 +0700

    login and register

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  isHidden: boolean = false;
 
+  hidden(): void {
+    this.isHidden = true;
+  }
+
+  noHidden(): void {
+    this.isHidden = false;
+  }
 }
+
