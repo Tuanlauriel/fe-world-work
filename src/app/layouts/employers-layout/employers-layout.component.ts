3356c1ee7b1a559c836bf9e076bb5098a55c2b3f
Nguyen Minh Hieu <ngmhieu1709@gmail.com>
2023-12-05 16:42:27 +0700

    Add sidebar for emlployeer layout

--- src/app/layouts/employers-layout/employers-layout.component.ts
+++ src/app/layouts/employers-layout/employers-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-employers-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, RouterLink],
+  imports: [HeaderComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './employers-layout.component.html',
   styleUrl: './employers-layout.component.scss'
 })
